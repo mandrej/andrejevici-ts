da91ae19b24b531f5d914731e981fc24d6a0d020
mandrej <milan.andrejevic@gmail.com>
2025-01-01 10:19:28 +0100

    pinia-plugin-persistedstate-2

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,6 @@
 import { defineStore } from '#q-app/wrappers'
 import { createPinia } from 'pinia'
+import { createPersistedStatePlugin } from 'pinia-plugin-persistedstate-2'
 
 /*
  * When adding new properties to stores, you should also
@@ -24,9 +25,9 @@ declare module 'pinia' {
 
 export default defineStore((/* { ssrContext } */) => {
   const pinia = createPinia()
+  const installPersistedStatePlugin = createPersistedStatePlugin()
 
   // You can add Pinia plugins here
-  // pinia.use(SomePiniaPlugin)
-
+  pinia.use((context) => installPersistedStatePlugin(context))
   return pinia
 })
